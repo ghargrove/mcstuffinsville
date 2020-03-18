d75db67883083ae7d7332c083856d42a507b91e6
Graham Hargrove <hargrove.graham@yahoo.com>
2020-03-17 22:26:29 -0400

    Added simple graphql server.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,34 @@
 import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express';
 
+const typeDefs = gql`
+  type Patient {
+    email: String!
+  }
+  type Query {
+    getPatients: [Patient!]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getPatients: () => {
+      return [
+        { email: 'test@email.com' },
+        { email: 'other@email.com' }
+      ]
+    }
+  }
+}
+
+const port = 3000
+const server = new ApolloServer({ resolvers, typeDefs })
 const app = express()
 
-const port = 3000;
+server.applyMiddleware({ app })
 
-app.get('/', (req, res) => {
-  res.send('Test response')
+app.get('/', (_, res) => {
+  res.redirect('/graphql')
 })
 
 app.listen(port, () => console.log(`Now listening on http://localhost:${3000}`))
