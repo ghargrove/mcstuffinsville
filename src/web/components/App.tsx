ed862e1a87733cbfe31621573663f4a6adc8fe72
Graham Hargrove <hargrove.graham@yahoo.com>
2020-03-18 01:59:55 -0400

    Dropped apollo-boost. Setup simple patient query.

--- src/web/components/App.tsx
+++ src/web/components/App.tsx
@@ -1,15 +1,56 @@
 import React from 'react'
 
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloProvider, useQuery } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import gql from 'graphql-tag'
+
+import { IPatient } from '../../server/store'
+
+interface IGetPatientResponse {
+  getPatient: IPatient
+}
+
+const getPatientQuery = gql`
+  query getPatient($id: Int!) {
+    getPatient(id: $id) {
+      id
+      email
+    }
+  }
+`
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql'
+  cache: new InMemoryCache({
+    addTypename: false
+  }),
+  link: new HttpLink({
+    uri: 'http://localhost:3000/graphql'
+  })
 })
+
+const Patient: React.FC = () => {
+  const { loading, error, data } = useQuery<IGetPatientResponse>(
+    getPatientQuery,
+    { variables: { id: 1 } }
+  )
+
+  if (error || (!loading && data === undefined)) {
+    return <div>Whoops</div>
+  }
+
+  if (loading) {
+    return <div>Loading patient...</div>
+  }
+
+  return <div>{JSON.stringify(data?.getPatient)}</div>
+}
 
 const App: React.FC = () => (
   <ApolloProvider client={client}>
     <div>React application</div>
+    <Patient />
   </ApolloProvider>
 )
 
