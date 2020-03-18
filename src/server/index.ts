2e9a57efe44ac73a58436c3880ab32181b9315e5
Graham Hargrove <hargrove.graham@yahoo.com>
2020-03-17 23:14:46 -0400

    Updated prettier

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
-import { ApolloServer, gql } from 'apollo-server-express';
-import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express'
+import express from 'express'
+
+import patients from './patients.json'
 
 const typeDefs = gql`
   type Patient {
@@ -17,27 +19,25 @@ const typeDefs = gql`
   type Query {
     getPatients: [Patient!]!
   }
-`;
+`
 
 const resolvers = {
   Query: {
-    getPatients: () => {
-      return [{ email: 'test@email.com' }, { email: 'other@email.com' }];
-    }
+    getPatients: () => patients
   }
-};
+}
 
-const port = 3000;
-const server = new ApolloServer({ resolvers, typeDefs });
-const app = express();
+const port = 3000
+const server = new ApolloServer({ resolvers, typeDefs })
+const app = express()
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app })
 
 app.get('/', (_, res) => {
-  res.redirect('/graphql');
-});
+  res.redirect('/graphql')
+})
 
 app.listen(port, () =>
   // tslint:disable-next-line:no-console
   console.log(`Now listening on http://localhost:${3000}`)
-);
+)
