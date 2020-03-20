54ae487c7370da8232bacd90963941372205d8d2
Graham Hargrove <hargrove.graham@yahoo.com>
2020-03-19 22:38:31 -0400

    Allow webpack to take env.

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,35 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    app: path.resolve(__dirname, 'src/web/index.tsx')
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'babel-loader' },
-      { test: /\.png$/, loader: 'file-loader'  }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: require('html-webpack-template'),
-      appMountId: 'app'
-    })
-  ],
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+module.exports = (env = {}) => {
+  const baseConfig = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: {
+      app: path.resolve(__dirname, 'src/web/index.tsx')
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: 'babel-loader' },
+        { test: /\.png$/, loader: 'file-loader' }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: require('html-webpack-template'),
+        appMountId: 'app'
+      })
+    ],
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx']
+    }
   }
+
+  return baseConfig
 }
