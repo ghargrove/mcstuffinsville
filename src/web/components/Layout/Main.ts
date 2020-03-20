eac7b65bec2cdb5c5877cd79d851d3c0dee72922
Graham Hargrove <hargrove.graham@yahoo.com>
2020-03-20 11:37:37 -0400

    Updated main tag.

--- src/web/components/Layout/Main.ts
+++ src/web/components/Layout/Main.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const Main = styled.div`
+const Main = styled.main`
   background-color: ${props => props.theme.colors.gray.gray000};
   margin: 70px 0 0 232px;
   width: 100%;
